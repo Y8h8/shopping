ac6d2690230c2e0d04bebd3ab2885db96f17904b
Y8h8 <2482910479@qq.com>
2023-12-08 15:31:32 +0800

    Changes

---
+++ src/com/bean/AddGoods.java
@@ -0,0 +1,19 @@
+package com.bean;
+
+import com.tool.Add;
+
+import java.util.Scanner;
+
+public class AddGoods implements Add {
+    @Override
+    public void add() {
+        Scanner s = new Scanner(System.in);
+        Goods goods = new Goods();
+        System.out.print("商品名:");
+        goods.setName(s.next());
+        System.out.print("数量:");
+        goods.setNumber(s.nextInt());
+        System.out.print("价格:");
+        goods.setPrice(s.nextDouble());
+    }
+}
